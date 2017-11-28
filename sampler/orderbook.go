dbd1ce92ccb592e403dced293294fa6c89a9b6bd
sharath <sharathramku@umass.edu>
2017-11-28 02:20:37 -0500

    wrote orderbook

--- sampler/orderbook.go
+++ sampler/orderbook.go
@@ -1,1 +1,19 @@
 package sampler
+
+import (
+	"net/http"
+	"encoding/json"
+)
+
+type Orderbook struct {
+	Name     string          `json:"name"`
+	Book     string          `json:"book"`
+	Asks     [][]interface{} `json:"asks"`
+	Bids     [][]interface{} `json:"bids"`
+}
+
+func (ob *Orderbook) updateEntries() {
+	response,_ := http.Get(ob.Book)
+	defer response.Body.Close()
+	json.NewDecoder(response.Body).Decode(&ob)
+}
