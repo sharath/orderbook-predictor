268f39efe7e12880c3f9033d91ddde512b706e04
sharath <sharathramku@umass.edu>
2017-11-28 03:12:34 -0500

    writing to file

--- main.go
+++ main.go
@@ -4,31 +4,34 @@ import (
 	"fmt"
 	"time"
 	slr "github.com/sharath/orderbook-predictor/sampler"
+	"os"
+	"path"
 )
 
 func sample(cb *slr.GDAX) {
+	file,_ := os.Create(path.Join("data", "file.txt"))
+	defer file.Close()
 	for {
-		time.Sleep(2 * time.Second)
+		time.Sleep(0.5*time.Second)
 		n := time.Now()
 		cb.UpdateTokens()
 		cb.UpdateOrderbook()
-		for i := 0; i < 2; i++ {
-			fmt.Print(n.Format("02-Jan-06, 15:04PM") + ", ")
-			fmt.Print(cb.Tokens[i].Name + ", ")
-			fmt.Println(cb.Tokens[i].Price)
-			fmt.Println("Bid:")
-			for i := 0; i < len(cb.Orderbook[i].Bids); i++ {
-				fmt.Print("Price:")
-				fmt.Println(cb.Orderbook[i].Bids[i][0])
-				fmt.Print("Amount:")
-				fmt.Println(cb.Orderbook[i].Bids[i][1])
+
+		for i := 0; i < len(cb.Tokens); i++ {
+			fmt.Fprint(file,n.Format("02-Jan-06, 15:04PM, "))
+			fmt.Fprint(file,cb.Tokens[i].Name + ", ")
+			fmt.Fprintln(file,cb.Tokens[i].Price)
+			for j := 0; j < len(cb.Orderbook[i].Bids); j++ {
+				fmt.Fprint(file,cb.Tokens[i].Name + ", Bid, Price:")
+				fmt.Fprint(file,cb.Orderbook[i].Bids[j][0])
+				fmt.Fprint(file,", Amount:")
+				fmt.Fprintln(file,cb.Orderbook[i].Bids[j][1])
 			}
-			fmt.Println("Ask:")
-			for i := 0; i < len(cb.Orderbook[i].Bids); i++ {
-				fmt.Print("Price:")
-				fmt.Println(cb.Orderbook[i].Bids[i][0])
-				fmt.Print("Amount:")
-				fmt.Println(cb.Orderbook[i].Bids[i][1])
+			for j := 0; j < len(cb.Orderbook[i].Asks); j++ {
+				fmt.Fprint(file,cb.Tokens[i].Name + ", Ask, Price:")
+				fmt.Fprint(file,cb.Orderbook[i].Asks[j][0])
+				fmt.Fprint(file,", Amount:")
+				fmt.Fprintln(file,cb.Orderbook[i].Asks[j][1])
 			}
 		}
 	}
@@ -36,6 +39,8 @@ func sample(cb *slr.GDAX) {
 
 func main() {
 	cb := slr.ConfigGDAX("config.json")
+	os.MkdirAll("data", os.ModePerm)
+
 	go sample(cb)
 	select {}
 }
