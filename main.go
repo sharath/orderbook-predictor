edd93b816f4e2375fc4875b176717d1142fabea5
sharath <sharathramku@umass.edu>
2017-12-12 08:28:09 -0500

    cleaner printing, added index for each poll

--- main.go
+++ main.go
@@ -13,52 +13,34 @@ func main() {
 	os.MkdirAll("data", os.ModePerm)
 	dataPath := path.Join("data", time.Now().Format("02-Jan-06.csv"))
 	file, _ := os.OpenFile(dataPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 777)
+	var index uint64 = 0
 	for range time.NewTicker(time.Second).C {
-		t := func() {
+		poll := func() {
 			if file.Name() != path.Join("data", time.Now().Format("02-Jan-06.csv")) {
 				file.Close()
 				file, _ = os.OpenFile(dataPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 777)
 			}
-			time.Sleep(time.Second*5)
-
 			n := time.Now()
 			cb.UpdateTokens()
 			cb.UpdateOrderbook()
-
 			for i := 0; i < len(cb.Tokens); i++ {
-				fmt.Fprintln(file,n.Format("02-Jan-06, 15:04:05.000PM"))
-				fmt.Fprint(file,cb.Tokens[i].Name + ", Market, ")
-				fmt.Fprintln(file,cb.Tokens[i].Price)
-
-				fmt.Println(n.Format("02-Jan-06, 15:04:05.000PM"))
-				fmt.Print(cb.Tokens[i].Name + ", Market, ")
-				fmt.Println(cb.Tokens[i].Price)
-
 				for j := 0; j < len(cb.Orderbook[i].Bids); j++ {
-					fmt.Fprint(file,cb.Tokens[i].Name + ", Bid, ")
-					fmt.Fprint(file,cb.Orderbook[i].Bids[j][0])
-					fmt.Fprint(file,", ")
-					fmt.Fprintln(file,cb.Orderbook[i].Bids[j][1])
-
-					fmt.Print(file,cb.Tokens[i].Name + ", Bid, ")
-					fmt.Print(file,cb.Orderbook[i].Bids[j][0])
-					fmt.Print(file,", ")
-					fmt.Println(file,cb.Orderbook[i].Bids[j][1])
+					line := fmt.Sprintf("%d, %s, %s, Bid, %s, %s", index, n.Format("15, 04, 05.000"), cb.Tokens[i].Name, cb.Orderbook[i].Bids[j][0], cb.Orderbook[i].Bids[j][1])
+					fmt.Println(line)
+					fmt.Fprintln(file, line)
 				}
+				line := fmt.Sprintf("%d, %s, %s, Market, %f", index, n.Format("15, 04, 05.000"), cb.Tokens[i].Name, cb.Tokens[i].Price)
+				fmt.Println(line)
+				fmt.Fprintln(file, line)
 				for j := 0; j < len(cb.Orderbook[i].Asks); j++ {
-					fmt.Fprint(file,cb.Tokens[i].Name + ", Ask, ")
-					fmt.Fprint(file,cb.Orderbook[i].Asks[j][0])
-					fmt.Fprint(file,", ")
-					fmt.Fprintln(file,cb.Orderbook[i].Asks[j][1])
-
-					fmt.Print(file,cb.Tokens[i].Name + ", Ask, ")
-					fmt.Print(file,cb.Orderbook[i].Asks[j][0])
-					fmt.Print(file,", ")
-					fmt.Println(file,cb.Orderbook[i].Asks[j][1])
+					line := fmt.Sprintf("%d, %s, %s, Ask, %s, %s", index, n.Format("15, 04, 05.000"), cb.Tokens[i].Name, cb.Orderbook[i].Asks[j][0], cb.Orderbook[i].Asks[j][1])
+					fmt.Println(line)
+					fmt.Fprintln(file, line)
 				}
 			}
 		}
-		go t()
+		go poll()
+		index++
 	}
 	select {}
 }
