f25058595e882d51de78fdff497b118779824278
sharath <sharathramku@umass.edu>
2017-12-13 00:41:42 -0500

    ?

--- analysis/bpg.go
+++ analysis/bpg.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 
 	"github.com/sjwhitworth/golearn/base"
-	"github.com/sjwhitworth/golearn/evaluation"
-	"github.com/sjwhitworth/golearn/knn"
+	_ "github.com/sjwhitworth/golearn/evaluation"
+	_ "github.com/sjwhitworth/golearn/knn"
 )
 
 func main() {
@@ -20,23 +20,4 @@ func main() {
 	// Print a pleasant summary of your data.
 	fmt.Println(rawData)
 
-	//Initialises a new KNN classifier
-	cls := knn.NewKnnClassifier("euclidean", "linear", 2)
-
-	//Do a training-test split
-	trainData, testData := base.InstancesTrainTestSplit(rawData, 0.50)
-	cls.Fit(trainData)
-
-	//Calculates the Euclidean distance and returns the most popular label
-	predictions, err := cls.Predict(testData)
-	if err != nil {
-		panic(err)
-	}
-
-	// Prints precision/recall metrics
-	confusionMat, err := evaluation.GetConfusionMatrix(testData, predictions)
-	if err != nil {
-		panic(fmt.Sprintf("Unable to get confusion matrix: %s", err.Error()))
-	}
-	fmt.Println(evaluation.GetSummary(confusionMat))
 }
